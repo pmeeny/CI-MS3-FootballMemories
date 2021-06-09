18375227bfce5214bfde9abeb30536f77309a666
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-06-09 11:56:49 +0000

    feat: Add mongo library imports

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def hello():
