d5a7278229e8867e48f9593bfbd775610fded291
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-06-16 12:18:09 +0000

    feat: Add get_categores route to return categories from the database

--- app.py
+++ app.py
@@ -89,6 +89,11 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
