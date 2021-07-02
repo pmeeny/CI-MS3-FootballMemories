9c2bfcff4ed09d1c36385477a843cf43e050adb9
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-02 15:49:54 +0000

    feat: Confirm password validation

--- football_memories/static/js/script.js
+++ football_memories/static/js/script.js
@@ -17,3 +17,17 @@
 function emptyCommentField() {
     document.getElementById("comment").value = ''
 }
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm-password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
