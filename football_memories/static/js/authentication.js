b94df96d5b003972ba434985fe23cce1d54e4bdc
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-07 14:17:36 +0000

    feat: New javascript file for authentication js

---
+++ football_memories/static/js/authentication.js
@@ -0,0 +1,29 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm-password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
