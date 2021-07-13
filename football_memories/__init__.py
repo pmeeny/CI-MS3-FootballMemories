2426589bad176a3dec206f9cb0d9735d9dc744d0
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-13 12:41:03 +0000

    refactor: Pep8 code updates

--- football_memories/__init__.py
+++ football_memories/__init__.py
@@ -13,20 +13,14 @@ from flask_paginate import Pagination, get_page_args
 if os.path.exists("env.py"):
     import env
 
-UPLOAD_FOLDER = '/workspace/CI-MS3-FootballMemories/static/images'
-IMAGE_PATH = '/static/images/'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['IMAGE_PATH'] = IMAGE_PATH
-
 mongo = PyMongo(app)
 mongo.init_app(app)
+
 
 def create_app():
 
