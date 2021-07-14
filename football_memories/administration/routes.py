d7b40b93fa6a8c8f6c181a21fef02d2a296c3b01
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-14 21:13:25 +0000

    feat: Display number of tournaments

--- football_memories/administration/routes.py
+++ football_memories/administration/routes.py
@@ -40,10 +40,12 @@ def privacy_policy():
 @administration.route("/dashboard")
 def dashboard():
     number_of_users = mongo.db.users.count()
+    number_of_tournaments = mongo.db.tournaments.count()
     number_of_memories = mongo.db.memories.count()
     number_of_comments = mongo.db.comments.count()
     return render_template("administration/dashboard.html",
                            number_of_users=number_of_users,
+                           number_of_tournaments=number_of_tournaments,
                            number_of_memories=number_of_memories,
                            number_of_comments=number_of_comments)
 
