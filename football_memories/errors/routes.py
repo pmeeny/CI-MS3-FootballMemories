b07d748a2f193ed8cc3265dc09bd48717cf820a5
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-26 17:23:59 +0100

    refactor: Error should use app for correct error routing

--- football_memories/errors/routes.py
+++ football_memories/errors/routes.py
@@ -1,17 +1,16 @@
 from flask import (render_template, Blueprint)
 
+from football_memories import app
 errors = Blueprint('errors', __name__)
 
-
-@errors.errorhandler(404)
+@app.errorhandler(404)
 def error_404(error):
     """
     Render the 404.html template in the case of a 404 error
     """
     return render_template('errors/404.html', error=error), 404
 
-
-@errors.errorhandler(500)
+@app.errorhandler(500)
 def error_500(error):
     """
     Render the 500.html template in the case of a 500 error
