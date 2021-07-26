5ce31e3ce46dfbad09dbc7bf48791faf4c0abbb6
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-07-26 16:48:11 +0100

    refactor: Remove unused imports and add comments to functions

--- football_memories/errors/routes.py
+++ football_memories/errors/routes.py
@@ -1,14 +1,19 @@
 from flask import (render_template, Blueprint)
-from flask import current_app
 
 errors = Blueprint('errors', __name__)
 
 
 @errors.errorhandler(404)
 def error_404(error):
+    """
+    Render the 404.html template in the case of a 404 error
+    """
     return render_template('errors/404.html', error=error), 404
 
 
 @errors.errorhandler(500)
 def error_500(error):
+    """
+    Render the 500.html template in the case of a 500 error
+    """
     return render_template('errors/500.html', error=error), 500
