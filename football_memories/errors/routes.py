f7bb28c787f8fd28e8edbbe65fb840ec88608210
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-08-04 21:21:14 +0000

    feat: Update route for 400, 401, 405 errors

--- football_memories/errors/routes.py
+++ football_memories/errors/routes.py
@@ -18,3 +18,27 @@ def error_500(error):
     Render the 500.html template in the case of a 500 error
     """
     return render_template('errors/500.html', error=error), 500
+
+
+@app.errorhandler(400)
+def error_400(error):
+    """
+    Render the 400.html template in the case of a 500 error
+    """
+    return render_template('errors/400.html', error=error), 400
+
+
+@app.errorhandler(401)
+def error_401(error):
+    """
+    Render the 401.html template in the case of a 401 error
+    """
+    return render_template('errors/401.html', error=error), 401
+
+
+@app.errorhandler(405)
+def error_405(error):
+    """
+    Render the 405.html template in the case of a 405 error
+    """
+    return render_template('errors/405.html', error=error), 405
