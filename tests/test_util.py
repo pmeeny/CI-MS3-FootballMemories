9066ea4f3ef60951a53a01debd289dc3e9046893
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-08-04 21:30:23 +0000

    refactor: Add comments to test cases

--- tests/test_util.py
+++ tests/test_util.py
@@ -9,27 +9,24 @@ from football_memories.util import util
 if os.path.exists("env.py"):
     import env
 
+
 def test_generateTimestamp():
+    """
+    This test asserts the timestamp is in the
+    correct format and length
+    """
     timestamp = util.generateTimestamp()
     assert (len(timestamp) == 20)
     assert(timestamp.startswith("2021"))
     assert (timestamp.endswith("_"))
-        
+
+
 def test_getMonthAndYear():
+    """
+    This test asserts the month and year string is in the
+    correct format and length
+    """
     month, year = util.getMonthAndYear()
     assert (len(month) == 2)
     assert (len(year) == 4)
     assert (year == "2021")
-        
-    #def test_setupPagination():    
-    #    offset, per_page, page = util.setupPagination()  
-    #    print (offset)
-    #    print (per_page)
-    #    print (page)
-        
-        #https://stackoverflow.com/questions/17375340/testing-code-that-requires-a-flask-app-or-request-context
-        #https://pythonhosted.org/Flask-Testing/
-        
-        #coverage run -m pytest
-        #coverage report -m
-        
