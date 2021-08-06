9c1902b4e267ec3e9f437bd8cd7da73735b490b6
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-08-06 10:00:43 +0000

    refactor: Set debug to False

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from football_memories import create_app
 if os.path.exists("env.py"):
     import env
 
+# Create an app
 app = create_app()
 
+# Run the app
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
