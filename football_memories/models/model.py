da5ec5e79bec35f274aaaad02be7d831f6f71028
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-08-09 11:55:23 +0000

    refactor: Add class comments

--- football_memories/models/model.py
+++ football_memories/models/model.py
@@ -2,6 +2,9 @@ from mongoengine import Document, IntField, StringField
 
 
 class Users(Document):
+    """
+    User collection stores the user information
+    """
     user_type = StringField(max_length=20, required=True)
     username = StringField(max_length=30, required=True, unique=True)
     password = StringField(max_length=30, required=True)
@@ -12,17 +15,26 @@ class Users(Document):
 
 
 class Tournaments(Document):
+    """
+    Tournaments collection stores the tournament information
+    """
     tournament_name = StringField(max_length=50, required=True, unique=True)
     tournament_image = StringField(max_length=100, required=True, unique=True)
 
 
 class Ratings(Document):
+    """
+    Ratings collection stores the rating information
+    """
     memory_id = StringField(max_length=30, required=True)
     memory_view_count = IntField(max_length=1, required=True)
     rating_created_by = StringField(max_length=30, required=True)
 
 
 class Comments(Document):
+    """
+    Comments collection stores the comment information
+    """
     memory_id = StringField(max_length=20, required=True)
     comment_text = StringField(max_length=100, required=True)
     comment_date = StringField(max_length=10, required=True)
@@ -30,6 +42,9 @@ class Comments(Document):
 
 
 class Memories(Document):
+    """
+    Memories collection stores the memories information
+    """
     tournament_image = StringField(max_length=100, required=True, unique=True)
     tournament_name = StringField(max_length=50, required=True)
     memory_name = StringField(max_length=50, required=True)
